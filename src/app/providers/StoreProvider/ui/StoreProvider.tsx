5d47ff5302de97c770b2dd96e315e1ce58ab2d60
petrovskyi_v <petrovskyi.webdev@gmail.com>
2024-12-21 20:53:21 -0600

    RP-2: Redux Toolkit

---
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -0,0 +1,21 @@
+import { type FC } from 'react'
+import { Provider } from 'react-redux'
+import { createReduxStore } from '../config/store'
+import { type IStateSchema } from '../config/stateSchema'
+
+interface StoreProviderProps {
+  children: React.ReactNode,
+  initialState?: IStateSchema
+}
+
+const StoreProvider: FC<StoreProviderProps> = ({ children, initialState }) => {
+  const store = createReduxStore(initialState)
+
+  return (
+    <Provider store={store}>
+     {children}
+    </Provider>
+  )
+}
+
+export default StoreProvider
