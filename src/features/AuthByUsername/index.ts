480a7e9fd25ef5be397b43970a8d73537fcbbcd2
petrovskyi_v <Petrovskyi.webdev@gmail.com>
2025-02-17 17:52:20 -0600

    Tweak

--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,7 +1,6 @@
 import LoginModal from './ui/LoginModal/LoginModal.feature'
 
 export { loginActions } from './model/slice/loginSlice'
-export { getLoginState } from './model/selectors/getLoginState/getLoginState'
 export { loginByUsername } from './model/services/loginByUsername'
 export { type LoginSchema, LoginErrors } from './model/types/loginSchema'
 export { LoginModal }
--- src/features/AuthByUsername/model/selectors/getLoginState/getLoginState.ts
+++
@@ -1,4 +0,0 @@
-import { type IStateSchema } from 'app/providers/StoreProvider/config/stateSchema'
-import { type LoginSchema } from '../../types/loginSchema'
-
-export const getLoginState = (state: IStateSchema): LoginSchema => state?.loginForm
